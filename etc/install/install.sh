3b7ca36204f18e940db1356eab5dd6ae2897e430
Bob Lannon <blannon@gmail.com>
2013-06-02 11:57:49 -0400

    initial commit

---
+++ etc/install/install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Script to set up a Django project on Vagrant.
+
+# Installation settings
+
+PROJECT_NAME=$1
+
+DB_NAME=$PROJECT_NAME
+VIRTUALENV_NAME=$PROJECT_NAME
+
+PROJECT_DIR=/home/vagrant/$PROJECT_NAME
+VIRTUALENV_DIR=/home/vagrant/.virtualenvs/$PROJECT_NAME
+
+PGSQL_VERSION=9.1
+
+# Need to fix locale so that Postgres creates databases in UTF-8
+cp -p $PROJECT_DIR/etc/install/etc-bash.bashrc /etc/bash.bashrc
+locale-gen en_GB.UTF-8
+dpkg-reconfigure locales
+
+export LANGUAGE=en_GB.UTF-8
+export LANG=en_GB.UTF-8
+export LC_ALL=en_GB.UTF-8
+
+# Install essential packages from Apt
+apt-get update -y
+# Python dev packages
+apt-get install -y build-essential python python-dev python-setuptools python-pip
+# Postgresql
+apt-get install -y postgresql-$PGSQL_VERSION libpq-dev
+# Dependencies for image processing with PIL
+apt-get install -y libjpeg62-dev zlib1g-dev libfreetype6-dev liblcms1-dev
+
+# postgresql global setup
+cp $PROJECT_DIR/etc/install/pg_hba.conf /etc/postgresql/$PGSQL_VERSION/main/
+/etc/init.d/postgresql reload
+
+# virtualenv global setup
+easy_install -U pip
+easy_install virtualenv virtualenvwrapper stevedore virtualenv-clone
+
+# bash environment global setup
+cp -p $PROJECT_DIR/etc/install/bashrc /home/vagrant/.bashrc
+su - vagrant -c "mkdir -p /home/vagrant/.pip_download_cache"
+
+# ---
+
+# postgresql setup for project
+createdb -Upostgres $DB_NAME
+
+# virtualenv setup for project
+su - vagrant -c "/usr/local/bin/virtualenv $VIRTUALENV_DIR && \
+    echo $PROJECT_DIR > $VIRTUALENV_DIR/.project && \
+    PIP_DOWNLOAD_CACHE=/home/vagrant/.pip_download_cache $VIRTUALENV_DIR/bin/pip install -r $PROJECT_DIR/requirements.txt"
+
+echo "workon $VIRTUALENV_NAME" >> /home/vagrant/.bashrc
+
+# Set execute permissions on manage.py, as they get lost if we build from a zip file
+chmod a+x $PROJECT_DIR/manage.py
+
+# Django project setup
+su - vagrant -c "source $VIRTUALENV_DIR/bin/activate && cd $PROJECT_DIR && ./manage.py syncdb --noinput && ./manage.py migrate"
